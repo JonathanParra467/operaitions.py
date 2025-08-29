ce5f81961279440a542bb9d31c402773887d0a1e
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-08-29 17:01:52 -0500

    Add files via upload

---
+++ operaitions.py
@@ -0,0 +1,10 @@
+
+#declare width and length and asighn numbers to them
+
+length = 8
+width = 6
+
+#calculate the length and width of the rectangle
+area = width * length
+
+print("A rectangle with lenght of", length, "a width of", width, "has a area of", area)
